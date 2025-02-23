e7c9da50404bccae33cd794cbecb0f03acd5322b
Bagus Oktaviadi <bagusoktaviadi1@gmail.com>
2025-02-23 19:05:02 +0700

    [update] checkpoint downloader

--- utils.py
+++ utils.py
@@ -10,7 +10,7 @@ class BinaryDownloader:
     def download(self, uri, timeout=None, headers={}, verify_ssl=True):
         """Downloads content while handling binary data properly"""
         response = requests.get(
-            uri, timeout=timeout, headers=headers, verify_ssl=verify_ssl
+            uri, timeout=timeout, headers=headers, verify=verify_ssl
         )
         # Assume binary data should be decoded as latin1 if not UTF-8
         try:
@@ -51,11 +51,10 @@ def parse_m3u8(paylist_url: str):
 
     # Download the video using ffmpeg
     random_string = str(uuid.uuid4())
-    output_filename = (
-        f"output/videos/{paylist_url.split('/')[-2].split('?')[0]}_{random_string}.mp4"
-    )
+    output_filename = f"output/videos/{random_string}.mp4"
     system(f'ffmpeg -i "{paylist_url}" -c copy {output_filename}')
 
     # if you want to write a file from its content
+    random_string = str(uuid.uuid4())
     makedirs(path.join(path.curdir, "output/m3u8"), exist_ok=True)
-    playlist.dump(f"output/m3u8/{paylist_url.split("/")[-1]}")
+    playlist.dump(f"output/m3u8/{random_string}.m3u8")
